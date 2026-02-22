ae9d230dfd60f2294c96cd4512ae980e12e05f7a
Erosdy <lefebvrevillette.rodolphe@gmail.com>
2026-02-22 22:18:33 +0100

    Changement du type de ticket pour le remplacer par la nouvelle enum

--- src/ticket/ticket.interface.ts
+++ src/ticket/ticket.interface.ts
@@ -1,10 +1,11 @@
 import {TicketPriorityEnum} from "./ticket-priority.enum";
-import {StateEnum} from "../enum/state.enum";
+import {TicketTypeEnum} from "./ticket-type.enum";
+
 
 export interface TicketInterface {
 	id: string;
-	type: StateEnum;
-	targetId: Id<_HasId & RoomObject>;	// TODO à changer, le type doit être garanti via le ticket
+	type: TicketTypeEnum;
+	targetId: Id<_HasId & RoomObject>;
 	priority: TicketPriorityEnum;
 	assignTo?: string;
 }
